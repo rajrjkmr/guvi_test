401a06386812273e5a9756afa763c659645f99d7
rajrjkmr <rajdrengineer@gmail.com>
2018-05-05 21:23:06 +0530

    hunt2

--- hunt1.py
+++ hunt1.py
@@ -2,12 +2,5 @@ num=input()
 d=raw_input()
 d=d.split(" ")
 d=d[0:num]
-u=[]
-for x in d:
-	if d.count(x)>1:
-		if x in u:
-			pass
-		else:
-			u.append(x)
-for x in sorted(u):
+for x in sorted(d)[::-1]:
 	print x,
